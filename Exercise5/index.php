1e94652171e138f24792fdab9f13ebfc913d0bf5
Carl Justine Noleal <justinenoleal@gmail.com>
2016-12-02 12:10:52 +0800

    Added the Exercise3 mypage.html previous codes

--- Exercise5/index.php
+++ Exercise5/index.php
@@ -15,7 +15,7 @@ if(isset($_GET['delete_id']))
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>CRUD Operations With PHP and MySql - By Cleartuts</title>
+<title>The Carl's Page</title>
 <script type="text/javascript">
 function edt_id(id)
 {
@@ -184,5 +184,79 @@ table td button:active
 </div>
 
 </center>
+</body>
+<body id ="page">  <center>
+<p style="color:white;font-size:200;">-----------------------------------------------------------------------------------------------------------------------------------------</p>
+
+<h1 style ="color:white;font-family:courier;"> <center> HELLO! WELCOME TO MY PERSONAL WEBSITE! <center> </h1>
+
+<table>
+  <tr>
+    <th style = "color:white;">Name:</th>
+  </tr>
+
+  <tr>
+    <td style = "color:white;">Carl Justine Noleal</td>
+  </tr>
+</table>
+
+<p style="color:white;font-size:200;">-----------------------------------------------------------------------------------------------------------------------------------------</p>
+
+<table>
+  <tr>
+    <th style = "color:white;">
+	Nicknames:
+    </th>
+  </tr>
+
+  <tr>
+    <td style = "color:white;">Carl, Justin, Jus, CJ</td>
+  </tr>
+</table>
+
+<p style="color:white;font-size:200;">-----------------------------------------------------------------------------------------------------------------------------------------</p>
+
+<table>
+  <tr>
+    <th style = "color:white;">Hobies:</th>
+    <th style = "color:white;"></th>
+  </tr>
+
+  <tr>
+    <td id="SW"; style = "color:white;"><center>What do I do when I'm inspired?</center>
+    <center><button type="button"; style = "background-color:black;color:White;font-family:courier;font-size:100%" onclick="document.getElementById('SW').innerHTML = 'Song Writing!'">Want to know?!</button></center>
+    </td>
+    <td><img src="SW.jpg" alt = "Song Writing" style ="width:550px;height:200;"></td>
+ </tr>
+
+  <tr>
+    <td id="AG"; style = "color:white;"><center>What do I use?</center>
+    <center><button type="button"; style = "background-color:black;color:White;font-family:courier;font-size:100%" onclick="document.getElementById('AG').innerHTML = 'Acoustic Guitar!'">Want to know?!</button></center>
+    </td>
+    <td> 
+	<img src="AG.jpg" alt = "Acoustic Guitar" style ="width:550px;height:200;">
+    </td>
+  </tr>
+
+  <tr>
+   <td id="Band"; style = "color:white;"> <center>What is my favorite band?</center>
+   <center> <button type="button"; style = "background-color:black;color:White;font-family:courier;font-size:100%" onclick="document.getElementById('Band').innerHTML = 'The 1975!'">Want to know?!</button></center>
+    </td>
+    <td>
+	<img id = "THE"; src="1975.jpg" alt = "The 1975" style ="width:550px;height:200;">
+    </td>
+  </tr>
+
+  <tr>
+   <td id="web"; style = "color:white;"> <center>What is my favorite website?</center>
+   <center> <button type="button"; style = "background-color:black;color:White;font-family:courier;font-size:100%" onclick="document.getElementById('web').innerHTML = '9gag I think? Hmm.'">Want to know?!</button></center>
+    </td>
+    <td>
+	<img id = "9gag"; src="9gag.png" alt = "9gag" style ="width:550px;height:200;">
+    </td>
+  </tr>
+</table>
+</center>
+</table>
 </body>
 </html>
