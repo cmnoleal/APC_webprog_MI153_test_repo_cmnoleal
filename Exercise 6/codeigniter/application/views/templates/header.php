33d59fcbe895994ffe0f8462101ae2bbebb8ff30
Carl Justine Noleal <justinenoleal@gmail.com>
2016-12-19 16:07:14 +0800

    added some designs for header.php

--- Exercise 6/codeigniter/application/views/templates/header.php
+++ Exercise 6/codeigniter/application/views/templates/header.php
@@ -2,6 +2,28 @@
         <head>
                 <title>CodeIgniter Tutorial</title>
         </head>
-        <body>
-                <h1>Simple CRUD</h1>
-                <p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
+<style>
+#body{
+	font-family:courier;
+	font-size:20px;
+	color:white;
+	background-color:black;
+}
+#title{
+	font-family:courier;
+	font-size:20px;
+	color:white;
+	background-color:black;
+}
+#links{
+		font-family:courier;
+	font-size:20px;
+	color:white;
+	background-color:black;
+}
+</style>
+        <body id = 'body'>
+                <h1 id = 'title'>POGI KO</h1>
+                <p id ='links'><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
+</body>
+</html>
