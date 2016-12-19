8890741e0d0272bb1edac37a6854df32c3afa6ad
Carl Justine Noleal <justinenoleal@gmail.com>
2016-12-19 17:32:46 +0800

    Made some editing for header.php

--- Exercise 6/codeigniter/application/views/templates/header.php
+++ Exercise 6/codeigniter/application/views/templates/header.php
@@ -24,6 +24,6 @@
 </style>
         <body id = 'body'>
                 <h1 id = 'title'><center>Who's viewing this?</center></h1>
-                <p id ='links'><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
+               <p id ='links'> <center><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></center></p>
 </body>
 </html>
