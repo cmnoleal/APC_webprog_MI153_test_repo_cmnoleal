2491a711b3ec30a119b529dcdf6feb2affc80a99
Carl Justine Noleal <justinenoleal@gmail.com>
2016-12-19 16:05:18 +0800

    Added some designs for header.php

--- Exercise 6/codeigniter/application/views/header.php
+++ Exercise 6/codeigniter/application/views/header.php
@@ -3,5 +3,5 @@
                 <title>CodeIgniter Tutorial</title>
         </head>
         <body>
-                <h1>Simple CRUD</h1>
+                <h1>SIMPLE LANG DI KO MATAPOS TAPOS</h1>
                 <p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
