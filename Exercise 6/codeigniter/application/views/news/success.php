d07963c6b7d5130def944031e8acc319df067651
Carl Justine Noleal <justinenoleal@gmail.com>
2016-12-19 16:06:14 +0800

    added some designs for success.php

--- Exercise 6/codeigniter/application/views/news/success.php
+++ Exercise 6/codeigniter/application/views/news/success.php
@@ -1,1 +1,17 @@
-<p>News added successfully!</p>
+<style>
+#body{
+	font-family:courier;
+	font-size:20px;
+	color:white;
+	background-color:black;
+}
+#Success {
+	font-family:courier;
+	font-size:20px;
+	color:white;
+	background-color:black;
+}
+</style>
+<body id = 'body'>
+<p id = 'Success'>News added successfully!</p>
+</body>
