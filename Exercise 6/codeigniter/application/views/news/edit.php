89ad828441f96d66bfbe4cd2b14e384d4ac0d854
Carl Justine Noleal <justinenoleal@gmail.com>
2016-12-19 17:31:31 +0800

    Made some editing for edit.php

--- Exercise 6/codeigniter/application/views/news/edit.php
+++ Exercise 6/codeigniter/application/views/news/edit.php
@@ -6,7 +6,7 @@
     <table>
         <tr>
             <td><label for="title">Title</label></td>
-            <td><input type="input" name="title" size="50" value="<?php echo $news_item['ALABANG'] ?>" /></td>
+            <td><input type="input" name="title" size="50" value="<?php echo $news_item['title'] ?>" /></td>
         </tr>
         <tr>
             <td><label for="text">Text</label></td>
