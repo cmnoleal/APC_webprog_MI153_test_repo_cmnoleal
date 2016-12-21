85c7f3e7834f5d6dc12a11e7efb6964fc9a8d568
Carl Justine Noleal <justinenoleal@gmail.com>
2016-12-21 15:13:35 +0800

    Fixed the inputs prior to the database.

--- Exercise 6/codeigniter/application/views/header.php
+++ Exercise 6/codeigniter/application/views/header.php
@@ -2,6 +2,16 @@
         <head>
                 <title>CodeIgniter Tutorial</title>
         </head>
+		<style>
+	#h1 {
+	font-size:100%;
+	font-family: courier;
+	border-collapse: collapse;
+	width: 100%;
+	border:solid #d0d0d0 1px;
+	padding:20px;
+		}
+		</style>
         <body>
-                <h1>SIMPLE LANG DI KO MATAPOS TAPOS</h1>
+                <h1 id = 'h1'>SIMPLE LANG DI KO MATAPOS TAPOS</h1>
                 <p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
